f0a2a6392cd93df22d94d83518bd26980cf1e0e4
jonathanribs <jonathanribers@hotmail.com>
2021-01-12 10:42:18 -0300

    Avançando até aula 4 - foi usado get, post e o primiero jsp

---
+++ Spring/gerenciador/WebContent/novaEmpresaCriada.jsp
@@ -0,0 +1,9 @@
+<%//java server page -> JSP//
+	//Isso é um scriplet
+	String nomeEmpresa = "Alura";
+	System.out.println(nomeEmpresa);
+%>
+
+<html><body> 
+Empresa <%=nomeEmpresa /*out.println (nomeEmpresa); //essa é a forma completa*/ %> cadastrada com sucesso 
+</body></http>				
--- Spring/gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ Spring/gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -13,9 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
-		String nomeEmpresa = request.getParameter("nome-;");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		System.out.println(banco.getEmpresas().get(0).getNome()); //Só para confirmar que foi executado e a lista criada
+		
 		PrintWriter out = response.getWriter();
 		out.println("<html><body> Empresa " + nomeEmpresa  +  " cadastrada com sucesso </body></http>");				
 	}
