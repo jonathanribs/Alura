3a541bc0c358d7d7b4d13469b248c04f193391fa
jonathanribs <jonathanribers@hotmail.com>
2021-01-14 07:35:00 -0300

    Usando o web.xml

--- Spring/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ Spring/gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -9,9 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
+@WebServlet(urlPatterns={"/oi","/oii"}, loadOnStartup=1) //Pode ser passado um vetor {} de patterns com mapeamento para esta classe. 
+								//loadOnStartup é para o Tomcat criar esta classe assim que se inicia, se não tiver esse parametro o Tomcat só inicia a classe quando ela é utitilizada (no caso seria ao buscar a url no navegador)
 public class OiMundoServlet extends HttpServlet{
 	
+	public OiMundoServlet() {
+		System.out.println("Criando Oi Mundo Servlet");
+	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
