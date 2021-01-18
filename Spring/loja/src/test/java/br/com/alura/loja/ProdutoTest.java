ccb07bfcd840ee0e3c5276eafed7aa25bf0b744a
jonathanribs <jonathanribers@hotmail.com>
2021-01-18 11:07:52 -0300

    Projeto usado no curso Maven: Gerenciamento de dependencias e build de aplicacoes Java

---
+++ Spring/loja/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ProdutoTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		Produto p = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", p.getNome());
+		Assert.assertEquals(BigDecimal.TEN, p.getPreco());
+	}
+
+}
