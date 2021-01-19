c3959e466ac1700006af8018737adf28d7d2a57b
jonathanribs <jonathanribers@hotmail.com>
2021-01-19 07:58:39 -0300

    Inserindo e Removendo dados

--- Spring/loja-virtual-repositorio/src/main/java/TestaConexao.java
+++ Spring/loja-virtual-repositorio/src/main/java/TestaConexao.java
@@ -1,13 +1,13 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
 
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "admin");
-		
+		ConnectionFactory criaConexao = new ConnectionFactory();
+		Connection connection = criaConexao.recuperarConexao();
+				
 		System.out.println("Fechando Conexão!");
 		connection.close();
 		
--- Spring/loja-virtual-repositorio/src/main/java/TestaListagem.java
+++ Spring/loja-virtual-repositorio/src/main/java/TestaListagem.java
@@ -1,5 +1,4 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,9 +7,10 @@ public class TestaListagem {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "admin");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
 		
-		Statement stm = con.createStatement();
+		Statement stm = connection.createStatement();
 		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO"); 
 		
 		ResultSet rst = stm.getResultSet();
@@ -24,7 +24,7 @@ public class TestaListagem {
 			System.out.println(descricao);
 		}
 		
-		con.close();
+		connection.close();
 		
 	}
 }
