20a448d69b6f1dc4a6bf50092ed6c65c5766d273
jonathanribs <jonathanribers@hotmail.com>
2021-01-22 00:21:50 -0300

    Mapeando Relacionamentos

--- Spring/projeto-jpa/src/main/java/br/com/alura/jpa/testes/CriaContaComSaldo.java
+++ Spring/projeto-jpa/src/main/java/br/com/alura/jpa/testes/CriaContaComSaldo.java
@@ -13,14 +13,28 @@ public class CriaContaComSaldo {
 		EntityManager em = emf.createEntityManager();
 
 		Conta conta = new Conta();
-		conta.setTitular("Juliano");
+		conta.setTitular("Márcia");
 		conta.setNumero(12345);
 		conta.setAgencia(54321);
-		conta.setSaldo(500.0);
+		conta.setSaldo(100.0);
 
 		em.getTransaction().begin();
 		em.persist(conta);
 		em.getTransaction().commit();
-
+		
+		em.close();
+		//Como EntityManager fechado, os objetos não são mais managed, ou seja, não estão mais sincronizados
+		
+		EntityManager em2 = emf.createEntityManager();
+		System.out.println("ID da conta da Marcia" + conta.getId());
+		conta.setSaldo(500.0); //Detached
+				
+		em2.getTransaction().begin();
+		
+		//Detached -> Managed
+		em2.merge(conta);
+		
+		em2.getTransaction().commit();
+		
 	}
 }
