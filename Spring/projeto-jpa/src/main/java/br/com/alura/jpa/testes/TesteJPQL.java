ad4be88558d75ebbaafd9243383b499027d67f85
jonathanribs <jonathanribers@hotmail.com>
2021-01-22 12:13:08 -0300

    TypedQuery

--- Spring/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
+++ Spring/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.alura.jpa.modelo.Conta;
 import br.com.alura.jpa.modelo.Movimentacao;
@@ -14,23 +15,24 @@ public class TesteJPQL {
 
 	public static void main(String[] args) {
 
-//		String query = "SELECT * FROM MOVIMENTACAO WHERE CONTA_ID = 2";
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
 		EntityManager em = emf.createEntityManager();
 		
-		//String jpql = "SELECT m FROM Movimentacao m WHERE m.conta.id = 2"; //a forma abaixo substitui o uso do ID, buscando diretamente por uma conta
 		Conta conta = new Conta();
 		conta.setId(2L);
-		String jpql = "SELECT m FROM Movimentacao m WHERE m.conta = :pConta"; //os 2 pontos indicam que é um parâmetro [o "p" é só por conveção, ajuda na leitura]
-		
-		Query query = em.createQuery(jpql);
-		query.setParameter("pConta", conta); //não necessário se a buscar for feita por meio do Id
+		String jpql = "SELECT m FROM Movimentacao m WHERE m.conta = :pConta order by m.valor desc"; 
+		//os 2 pontos indicam que é um parâmetro [o "p" é só por conveção, ajuda na leitura]
+
+		//antes havia um aviso de que o tipo não era testado, inclusive sendo possível passar na JPQL uma classe diferente de Movimentacao, levando a quebra quando o resultado fosse utilizado
+		//para evitar este problema é usado a Query "tipada"
+		TypedQuery<Movimentacao> query = em.createQuery(jpql, Movimentacao.class); 
+		query.setParameter("pConta", conta); 
 		
 		List<Movimentacao> restulList = query.getResultList();
 		
 		for (Movimentacao movimentacao : restulList) {
 			System.out.println("Descrição: " + movimentacao.getDescricao());
+			System.out.println("Valor: " + movimentacao.getValor());
 			System.out.println("Tipo: " + movimentacao.getTipoMovimentacao());
 		}
 		
