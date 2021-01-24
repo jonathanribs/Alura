13a9cc5cccc566f42b6326b21a1881ea1997e1ab
jonathanribs <jonathanribers@hotmail.com>
2021-01-24 00:25:44 -0300

    Organizando o Código (Descobri que o código da SomaDasMovimentacoes estava errado)

--- Spring/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
+++ Spring/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -1,25 +1,13 @@
 package br.com.alura.jpa.testes;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import br.com.alura.jpa.modelo.Conta;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
 
 public class TestaSomaDasMovimentacoes {
 	public static void main(String[] args) {
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-		EntityManager em = emf.createEntityManager();
+		MovimentacaoDao dao = new MovimentacaoDao();
 		
-		Conta conta = new Conta();
-		conta.setTitular("Leonardo");
-		conta.setNumero(1234);
-		conta.setAgencia(4321);
-		
-		em.getTransaction().begin();	
-		em.persist(conta);
-		em.getTransaction().commit();
+	    System.out.println("A soma das movimentacoes é: " + dao.getSomaDasMovimentacoes());
 		
 	}
 }
