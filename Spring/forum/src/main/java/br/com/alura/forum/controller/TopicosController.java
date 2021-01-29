032088594449d60ff2fe2fd73fc15fd4556bc37b
jonathanribs <jonathanribers@hotmail.com>
2021-01-29 09:00:54 -0300

    Cadastrando Topicos

--- Spring/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Spring/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,20 +3,30 @@ package br.com.alura.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	//@RequestMapping(value="/topicos", method=RequestMethod.GET)
+	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		if (nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
@@ -25,6 +35,13 @@ public class TopicosController {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDto.converter(topicos);
 		}
+	}
+	
+	//@RequestMapping(value="/topicos", method=RequestMethod.POST)
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {//a anotaçao é pro Spring entender que está pegando o dado da requisição e não como parâmetros da URL que seria o padrão
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
 	}
 	
 }
