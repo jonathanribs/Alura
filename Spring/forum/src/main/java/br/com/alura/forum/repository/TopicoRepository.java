13da9f71476c67c450f523770fd84bdcdfcb4b80
jonathanribs <jonathanribers@hotmail.com>
2021-01-29 08:42:03 -0300

    Consulta por Filtros

--- Spring/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ Spring/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,18 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
+	List<Topico> findByCursoNome(String nomeCurso); //em caso de ambiguidade (tendo um atributo cursoNome na Entity Topico, por exemplo), para acessar o relacionamento curso e lá dentro o atributo nome, é só usar underline Curso_Nome
+
+	//Exemplo caso queira definir um nome personalizado para o método (perdemos a vantagem do Spring montar a query automaticamente)
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
