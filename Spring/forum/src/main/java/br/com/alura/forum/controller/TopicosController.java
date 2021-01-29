be5edbe80ca085b5c75780e313fd9f00e13a3ad2
jonathanribs <jonathanribers@hotmail.com>
2021-01-29 09:09:46 -0300

    Devolvendo um código Http

--- Spring/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Spring/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
@@ -25,7 +28,6 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
-	//@RequestMapping(value="/topicos", method=RequestMethod.GET)
 	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		if (nomeCurso == null) {
@@ -37,11 +39,13 @@ public class TopicosController {
 		}
 	}
 	
-	//@RequestMapping(value="/topicos", method=RequestMethod.POST)
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form) {//a anotaçao é pro Spring entender que está pegando o dado da requisição e não como parâmetros da URL que seria o padrão
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {//a anotaçao é pro Spring entender que está pegando o dado da requisição e não como parâmetros da URL que seria o padrão
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 	
 }
