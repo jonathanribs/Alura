178550779144fd2b6cad874a1c74c2127c8a3819
jonathanribs <jonathanribers@hotmail.com>
2021-01-29 08:26:27 -0300

    Spring Data JPA: utilizando repository

--- Spring/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Spring/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,25 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
-@Controller
+@RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida", "Duvida com String", new Curso("Spring", "Programação"));
-		
-		return Arrays.asList(topico, topico, topico);
+	public List<TopicoDto> lista() {
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDto.converter(topicos);
 	}
 	
 }
