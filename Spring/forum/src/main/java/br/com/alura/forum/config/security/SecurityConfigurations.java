50dfd219abefcaf662a810cd55cd564017786a63
jonathanribs <jonathanribers@hotmail.com>
2021-01-30 20:00:25 -0300

    Liberando acesso a métodos específicos

--- Spring/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ Spring/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,11 +1,34 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+	
+	//Configurações de autenticação (login, etc...)
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+	
+	//Configurações de autorização (quem acessa o que)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		
+	}
+	
+	//Configurações de recursos estatísticos (requisição para js, css, imagens, etc...)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
 
 }
