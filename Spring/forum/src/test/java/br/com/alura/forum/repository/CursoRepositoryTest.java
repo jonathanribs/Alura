41ac85c1ab2a29c901573d098b303618155aaef0
jonathanribs <jonathanribers@hotmail.com>
2021-02-15 09:21:45 -0300

    Testando o repository com outro banco de dados

--- Spring/forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ Spring/forum/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,21 +5,36 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE) //Necessário se não utilizar o h2 ou outro banco de dados em memoria
+@ActiveProfiles("test")
  public class CursoRepositoryTest {
 
 	@Autowired
 	private CursoRepository repository;
 	
+	@Autowired
+	private TestEntityManager em;
+	
 	@Test
 	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
 		String nomeCurso = "HTML 5";
+		
+		//Agora que está utilizando um banco de dados vazio, é necessário popular de acordo com o cenário para não haver erros no teste
+		Curso html5 = new Curso();
+		html5.setNome("nomeCurso");
+		html5.setCategoria("Programacao");
+		em.persist(html5);	
+		
 		Curso curso = repository.findByNome(nomeCurso);
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
