34472e114203f618a51da70ee9f9f256a280a8d3
jonathanribs <jonathanribers@hotmail.com>
2021-04-05 16:33:35 -0300

    Organizando as classes

--- Java/escola/src/test/java/br/com/alura/escola/aluno/TelefoneTest.java
+++ Java/escola/src/test/java/br/com/alura/escola/aluno/TelefoneTest.java
@@ -1,8 +1,10 @@
-package escola;
+package br.com.alura.escola.aluno;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import br.com.alura.escola.aluno.Telefone;
 
 class TelefoneTest {
 
