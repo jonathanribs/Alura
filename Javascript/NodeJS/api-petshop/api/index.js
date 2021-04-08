22418245f2e46a5bc8224a0286c978ad82056203
jonathanribs <jonathanribers@hotmail.com>
2021-04-08 11:10:06 -0300

    Trabalhar com Json

--- Javascript/NodeJS/api-petshop/api/index.js
+++ Javascript/NodeJS/api-petshop/api/index.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
+//const bodyParser = require('body-parser')
+
+//Definir json como padrão para nossa api
+//app.use(bodyParser.json()) //bodyParser ficou como deprecated, então foi substituido pelas 2 linhas abaixo
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(3000, () => console.log('A API está funcionando!'))
