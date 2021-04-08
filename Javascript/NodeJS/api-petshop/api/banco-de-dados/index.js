57a32125c5d1fedfc5480b0b2247e6342d2ea521
jonathanribs <jonathanribers@hotmail.com>
2021-04-08 11:18:12 -0300

    Configurando acesso ao banco de dados

---
+++ Javascript/NodeJS/api-petshop/api/banco-de-dados/index.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+
+const instancia = new Sequelize(
+    'petshop',
+    'root',
+    '123456',
+    {
+        host: '127.0.0.1',
+        dialect: 'mysql'
+    }
+)
+
+module.exports = instancia
